f011d112d4f99e524fd1f97c611af63820d916db
thomenr <thomenr@health.missouri.edu>
2025-04-22 09:10:53 -0500

    auto commit

--- XenonAnalysis.py
+++ XenonAnalysis.py
@@ -1,2 +1,17 @@
 ## -- Code for compliation of data for analysis and export of PACs compatible dicoms  -- ##
 
+calibration_twix = 
+
+xenon_pre_folder = 
+xenon_pos_folder = 
+
+mask_folder = 
+
+xenoview_pre_folder = 
+xenoview_pos_folder = 
+
+xenoview_pre_pdf = 
+xenoview_pos_pdf = 
+
+GX_folder = 
+GX_pdf = 
